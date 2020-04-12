0a9e61a7fa1208a8c9c32d5399ad6d36d3d21429
Salma Zakzouk <s.hossameldin@nu.edu.eg>
2020-04-13 00:17:29 +0200

    Generating random numbers for price,total,...

--- backend/books.py
+++ backend/books.py
@@ -1,9 +1,10 @@
 
 import csv
+import random 
 
 Books=[]
 class Book:
-    def __init__(self,isbn,title,author,year,total,sold,borrowed):
+    def __init__(self,isbn,title,author,year,total,sold,borrowed,available,price):
         self.isbn = isbn
         self.title = title
         self.author = author
@@ -11,26 +12,30 @@ class Book:
         self.total = total
         self.sold = sold
         self.borrowed = borrowed
+        self.available= available
+        self.price = price
         self.ratings = []
         self.reviews = []
     
-    def EditBook_Basic(self,isbn,title,author,year):
+    def EditBook_Basic(self,isbn,title,author,year,price):
         self.isbn = isbn
         self.title = title
         self.author = author
         self.year = year
+        self.price = price
     
-    def EditBook_Num(self,total,sold,borrowed):
+    def EditBook_Num(self,total,sold,borrowed,available):
         self.total = total
         self.sold = sold
         self.borrowed = borrowed
+        self.available = available
 
     def Rating(self,user, rating):
         new = {"User": user, "Rating": rating}
         self.ratings.append(new)
 
     def Review(self,user, review):
-        new = {"User": user, "Review": rating}
+        new = {"User": user, "Review": review}
         self.review.append(new)    
         
 
@@ -41,18 +46,30 @@ def main():
     reader =csv.reader(f)
     #create book objects
     for isbn,title,author,year in reader:
+
+        price = round(random.uniform(50,400),2)
+        total = random.randrange(100, 501, 1)
+        sold = random.randrange(0, 501, 1)
+        borrowed = random.randrange(0, 501, 1)
+        available = random.randrange(0, 501, 1)
+
+        while total != sold + borrowed + available:
+            sold = random.randrange(0, 501, 1)
+            borrowed = random.randrange(0, 501, 1)
+            available = random.randrange(0, 501, 1)  
+        
         if year != "year":        
-            book=Book(isbn,title,author,year)            
+            book=Book(isbn,title,author,year,total,sold,borrowed,available,price)            
             #list of book objects
             Books.append(book)
     
     # show list of Books
     for book in Books:
-        print ('ISBN: %s - Title: %s - Author: %s - Year: %s' % (book.isbn,book.title,book.author,book.year))
+        print ('ISBN: %s - Title: %s - Author: %s - Year: %s - Total: %d - Sold: %d - Borrowed: %d - Available: %d - Price: %f' % (book.isbn,book.title,book.author,book.year,book.total,book.sold,book.borrowed,book.available,book.price))
         
     print("done")            
     
 
 
 if __name__ == "__main__":
-    main()p
+    main()
