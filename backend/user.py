7321fc9fe14cf4aadad1683129f2814e959ba8bd
MWMAA <mahmoudamer95.bird@gmail.com>
2020-04-13 13:38:51 +0200

    .

--- backend/user.py
+++ backend/user.py
@@ -1,34 +1,38 @@
 from .util import dateValidator, _hashed, _verify_email
 
 
-class Author:
-    '''Author class is resposnible for the Authors's data modification and managment
+class User:
+    '''Author class is resposnible for the Authos's data modification and managment
     Atribs:
         ID: Int
         Name: String
         Date of birth (dob): Int:EPOCH
         List of books (lob): List<Int:ID>
-        Genres: List<String>
         Email: str
         Password (pwd): str
+        Borrowed books: List<Int:ID>
+        Bought books: List<Int:ID>
+        Pending books: List<Int:ID>
+        Rated books => ratings Dict:{Int:ID-Book : Int:Rating1-5}
+        Payment method: <<<UNSURE>>> 
+        Savings in account 
+        Address: String/Pair<Float, Float> ← coordinates?
+
 
     Methods:
         #ADDING => adding the author is done when the class instance is initiated
-        addGenre()
-        addBook()
+        addShelf() later on
+        addBookToShelf() later on
 
         #EDITING
-        editGenre()
-        editBook()
         editName()
         editDob()
         editPwd()
         editEmail()
 
         #REMOVING
-        removeGenre()
-        removeBook()
-        removeAuthor()
+        deleteShelf() later on
+        removeUser()
     '''
 
     def __init__(self, email, pwd, name='Jay', dob=None):
@@ -36,7 +40,6 @@ class Author:
         self.dob = dateValidator(dob)
         self.email = _verify_email(email)
         self.pwd = _hashed(pwd)
-        self.genre = []
         self.lob = []
 
     def addGenre(self, data):
@@ -54,23 +57,6 @@ class Author:
     def removeGenre(self, data):
         filter(lambda genre: genre == data, self.genre)
         return self.genre
-
-    def addBook(self, data):
-        if(isinstance(data, str)):
-            self.lob.append(data)
-        else:
-            print('Data not a string')
-        return self.lob
-
-    def editBook(self, equal, change):
-        for i in range(len(self.lob)):
-            if self.lob[i] == equal:
-                self.lob[i] = change
-        return self.lob
-
-    def removeBook(self, data):
-        filter(lambda book: book == data, self.lob)
-        return self.lob
 
     def editName(self, data):
         self.name = data
