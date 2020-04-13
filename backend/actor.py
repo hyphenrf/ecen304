ff889f12c8ac083bef34bc5dbfdfc8e96231afdc
Hazem M <hyphens@pm.me>
2020-04-13 13:06:50 +0200

    actually making Admin class work..

--- backend/actor.py
+++ backend/actor.py
@@ -9,7 +9,7 @@ Classes:
     - User
 """
 
-from util import verify_email, hashed
+from .util import _verify_email, _hashed
 
 
 
@@ -31,24 +31,38 @@ class Admin():
     def __init__(self, email: str, passwd: str, name= "Jon Doe"):
         
         # assertion checks. Will remove on external interfacing
-        assert verify_email(email) == True, "Invalid Email property"
+        assert _verify_email(email) == True, "Invalid Email property"
         assert isinstance(passwd, str), "Password must be a string"
         assert isinstance(name, str), "Name must be a string"
 
-        self.email = email.trim()
-        self.passwd = hashed(passwd)
+        self.email = email.strip()
+        self.passwd = _hashed(passwd)
         self.name = name
         self.id = self.__inc()
 
     @classmethod
     def __inc(cls):
         cls._adm_count += 1
-        return int(__ADM_SALT + str(cls._adm_count))
+        return int(cls.__ADM_SALT + str(cls._adm_count))
 
-    def del_obj(obj_id: int):
+    def del_obj(self, obj_id: int):
         # Should check if obj is in runtime and delete it
         # Should check if obj is in DB and delete it
         pass
+
+    def get_info(self) -> dict:
+        return {
+            "name": self.name,
+            "email": self.email,
+            "passwd": "[REDACTED]"
+        }
+    
+    def edit_info(self, inf: dict):
+        self.__init__(
+            inf.get("email") or self.email,
+            inf.get("passwd") or self.passwd,
+            inf.get("name") or self.name
+        )
 
 
 
