658585dc13667eb8e1c2c33a78483f5b043766b6
AyaSafan <ayasafan16@gmail.com>
2020-04-29 22:59:23 +0200

    SQL phase 4 -Aya

--- backend/create.py
+++ backend/create.py
@@ -1,11 +1,11 @@
-"""
+
 import os
 
 from flask import Flask, render_template, request
 from modelsBooks import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://nynvjnzdwddjcx:09d28298ab43f17d54fb573194bdde2962a0b08f823369f98c014a0df1bc94c4@ec2-34-202-7-83.compute-1.amazonaws.com:5432/dabb7iss8h6876"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ayasafan:25399@localhost/books"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -15,4 +15,3 @@ def main():
 if __name__ == "__main__":
     with app.app_context():
         main()
-"""
--- backend/import.py
+++ backend/import.py
@@ -6,7 +6,7 @@ from modelsBooks import *
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://nynvjnzdwddjcx:09d28298ab43f17d54fb573194bdde2962a0b08f823369f98c014a0df1bc94c4@ec2-34-202-7-83.compute-1.amazonaws.com:5432/dabb7iss8h6876"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ayasafan:25399@localhost/books"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
