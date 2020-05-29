915d6122f49335c13071883fe562d69820cd0927
Hazem M <hyphens@pm.me>
2020-05-29 19:13:05 +0200

    queries interface

---
+++ queries.py
@@ -0,0 +1,22 @@
+'''
+need an interface here
+'''
+def admins():
+    pass
+
+def users(sort=None, match=None):
+    pass
+
+def actor_by_email(email):
+    pass
+
+def update(object, id, fields):
+    pass
+
+def delete(object, id):
+    pass
+
+def add(object, id, fields):
+    pass
+
+
